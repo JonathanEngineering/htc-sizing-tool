9a6ad9cf3223538c1467ae8985c7f6e0e3c1eff0
Jonathan <156606707+JonathanEngineering@users.noreply.github.com>
2026-04-19 16:23:05 -0600

    Update and rename app.js to data.py
    Revamp 1

---
+++ data.py
@@ -0,0 +1,17 @@
+feedstocks = {
+    "wood": {
+        "hhv_raw": 18,      # MJ/kg
+        "upgrade": 1.3,
+        "yield": 0.6
+    },
+    "food_waste": {
+        "hhv_raw": 15,
+        "upgrade": 1.2,
+        "yield": 0.5
+    },
+    "algae": {
+        "hhv_raw": 20,
+        "upgrade": 1.25,
+        "yield": 0.55
+    }
+}
